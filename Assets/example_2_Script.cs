24a898529984470c31fd7dabff15388d5683de5a
GibWorkstation <flandre8233@gmail.com>
2018-11-21 11:50:19 +0800

    add encvar

---
+++ Assets/example_2_Script.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+//using System.Runtime.InteropServices;
+using gibgibEncrypt.Encrypt;
+using gibgibEncrypt.EncVar;
+using gibgibEncrypt.Cipher;
+
+public class example_2_Script : MonoBehaviour {
+[SerializeField]
+Text showtext;
+
+int showMaxHP;
+int showHP;
+
+[SerializeField]
+int MaxHP;
+[SerializeField]
+int HP;
+	void Start () {
+		HP = 75;
+		MaxHP = 100;
+	}
+	void Update () {
+		SyncHPData();
+		string result = showHP + " / " + showMaxHP;
+		showtext.text = result;
+		if(Input.GetKeyDown(KeyCode.N) ){
+			HP++;
+		}
+		if(Input.GetKeyDown(KeyCode.M) ){
+			HP--;
+		}
+	}
+	void SyncHPData(){
+		showHP = HP;
+		showMaxHP = MaxHP;
+	}
+}
+
+
